282ba1499c3ded96fb6096a5e5e061f85e346abb
ElissonLimaVale <elissonllima1@gmail.com>
2021-03-23 20:38:51 -0300

    finalizado movimentação do menu, layout para celular e redirecionamento de download

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,116 @@
 
 // Variaveis globais
-var color = document.getElementById("select-color").value;
-var div = null;
-var countWidth = document.getElementById("select-linha").value;
-var size = ((window.innerWidth * 100) / 90) / countWidth;
-var countHeight =  window.innerHeight / size ;
-var multcolor = false;
-var download = document.getElementById("download");
-var img = new Image();
-var touch = window.innerWidth > 700 ? "mousedown": "touchmove";
+let color = document.getElementById("select-color").value,
+	defaultColor = "rgb(255, 255, 255);",
+	countWidth = document.getElementById("select-linha").value,
+	size = window.innerWidth / countWidth,
+	countHeight =  window.innerHeight / (size - 1),
+	elements = null,
+	multElement = false,
+	download = document.getElementById("download"),
+	img = new Image(),
+	evento = window.innerWidth > 700 ? "mousedown": "touchmove";
 
+
+function main() {
+	inserir();
+	resizing();
+}
+
+function inserir() {
+	let loop = countHeight * countWidth;
+	for(let b = 1; b <= loop; b++){
+		elements = document.createElement('element');
+		elements.className = "element";
+
+		elements.addEventListener("mousedown", (event) => {
+			event.preventDefault ? event.preventDefault() : event.returnValue = false;
+			colorir(event.path[0], true);
+		}, false);
+
+		elements.addEventListener("mouseover", (event) => {
+			event.preventDefault ? event.preventDefault() : event.returnValue = false;
+			colorir(event.path[0], multElement);
+		}, false);
+		document.getElementById('corpo').appendChild(elements);
+	}
+}
+
+function resizing () {
+	size = window.innerWidth / countWidth;
+	countHeight = window.innerHeight / (size - 1);
+	document.getElementById("corpo").style = "width:" + window.innerWidth +"px";
+	document.getElementById("corpo").style = "height:" + window.innerHeight +"px";
+	
+	document.getElementById("corpo").addEventListener(evento, (event) => { 
+		multElement = true;
+	}, false);
+	document.getElementById("corpo").addEventListener("mouseup", () => { 
+		multElement = false;
+	}, false);
+	
+	
+	document.querySelectorAll('.element').forEach((element) => {
+		element.style = "width: "+ size + "px;height: "+ size+"px;background-color:"+ defaultColor;
+	});
+	
+}
+function colorir(element, multitens){
+	if(multitens){
+		element.style = "border: none;margin: 0px;background-color: "+ color +";width: "+ size +"px; height: "+ size +"px;"; 
+	}
+}
+
+function resetmenu(){
+	document.querySelectorAll(".all").forEach((element) => {
+		element.style = "background-color: rgb(252, 252, 252);";
+	});
+}
+
+function salvar() {
+	LoadShow();
+	html2canvas(document.querySelector("#corpo"), {
+		"logging": true, //Habilita os logs
+		"useCORS": true
+	}).then(function(canvas) {
+		img.onload = () => {
+			onload = null;
+			document.getElementById("image").appendChild(img);
+			LoadHide();
+		};
+		img.style = "width: 100%; height: 100%;";
+		img.src = canvas.toDataURL("image/png");
+		download.href = canvas.toDataURL("image/octet-stream");
+		download.download = "DrawnImage.png";
+	});
+	document.getElementById("image").style = "top: 2vh;";
+	document.getElementById("save-buttons").hidden = false;
+}
+
+//cancelar download
+document.getElementById("cancelar").addEventListener("click", () => {
+	document.getElementById("image").style = "top: -1200px";
+	document.getElementById("save-buttons").hidden = true;
+});
+//salvar 
+document.getElementById("salvar").addEventListener("click", () => {
+	window.open(img,"_blank");
+	
+	document.getElementById("image").style = "top: -1200px";
+	document.getElementById("save-buttons").hidden = true;
+});
+
+
+
+
+//Fecha o menu 
+document.getElementById("hidden-menu").addEventListener("click", () => {
+	document.querySelectorAll(".item-menu").forEach((element) => {
+		element.hidden = !element.hidden;
+	});
+});
+
+//Métodos do menu
 document.getElementById("save-buttons").hidden = true;
 
 // methodods
@@ -21,10 +122,7 @@ document.getElementById("select-color").addEventListener("change", () => {
 
 document.getElementById("select-linha").addEventListener("change", (e) => {
 	countWidth = document.getElementById("select-linha").value;
-	countHeight =  1200 / size;
-	inserir();
-	resizing();
-	init();
+	main();
 });
 
 document.getElementById("add").addEventListener("click", (e) => {
@@ -40,13 +138,13 @@ document.getElementById("dell").addEventListener("click", (e) => {
 });
 
 document.getElementById("delleteall").addEventListener("click", (e) => {
-	document.querySelectorAll(".div").forEach((element) => {
+	document.querySelectorAll(".element").forEach((element) => {
 		element.style = "border: none;margin: 0px;background-color: #fff;width: "+ size +"px; height: "+ size +"px;"; 
 	});
 });
 document.getElementById("more").addEventListener("click", () => {
 	countHeight += 10;
-	init();
+	main();
 });
 document.getElementById("quad").addEventListener("click", () => {
 	alert("Funcionalidade em desenvolvimento!");
@@ -54,97 +152,36 @@ document.getElementById("quad").addEventListener("click", () => {
 document.getElementById("save").addEventListener("click", salvar);
 
 
-function inserir() {
-	for(let a = 0; a <= countHeight; a++){
-		for(let b = 0; b <= countWidth; b++){
-			div = document.createElement('div');
-			div.className = "div";
-			document.getElementById('corpo').appendChild(div);
-		}
+// mover menu;
+let positionX = 10;
+let positionY = 10;
+let menu = document.getElementById("menu");
+let move = false;
+
+function movestart(e){
+	move = true;
+	positionX = e.pageX - menu.offsetLeft;
+	positionY = e.pageY - menu.offsetTop;
+	menu.addEventListener("mousemove", movemenu, false);
+	menu.addEventListener("mouseup", stopmenu);
+}
+function movemenu(e){
+	if(move){
+		menu.style.left = (e.pageX - positionX) + "px";
+		menu.style.top = (e.pageY - positionY) + "px";
 	}
 }
-
-function resizing () {
-	if(window.innerWidth > 700){
-		document.getElementById("corpo").addEventListener(touch, () => { 
-			multcolor = true;
-		});
-		document.getElementById("corpo").addEventListener("mouseup", () => { 
-			multcolor = false;
-		}, false);
-	}
-
-	let background = null;
-	size = ((window.innerWidth * 100) / 90) / countWidth;
-	document.querySelectorAll('.div').forEach((element) => {
-		background = !!!element.style.backgroundColor ? "rgb(255, 255, 255)": element.style.backgroundColor;
-		element.style = "background-color: "+ background +";border: none;margin: 0px;width: "+ size +"px; height: "+ size +"px;";
-	});
+function stopmenu(e){
+	move = false;
 	
 }
+menu.addEventListener("mousedown", movestart);
+menu.addEventListener("mouseout", stopmenu);
 
-function init(){
-	inserir();
-	document.querySelectorAll('.div').forEach((element) => {
-		element.addEventListener(touch, (event) => {
-			event.preventDefault ? event.preventDefault() : event.returnValue = false;
-			//alert();
-			colorir(event.path[0], true);
-		});
-		element.addEventListener("mouseover", (event) => {
-			event.preventDefault ? event.preventDefault() : event.returnValue = false;
-			colorir(event.path[0], multcolor);
-		});
-	});
-	resizing();
+function LoadShow() {
+	document.getElementById("load-area").hidden = false;
 }
-
-function colorir(element, multitens){
-	if(multitens){
-		element.style = "border: none;margin: 0px;background-color: "+ color +";width: "+ size +"px; height: "+ size +"px;"; 
-	}
+function LoadHide() {
+	document.getElementById("load-area").hidden = true;
 }
-
-function resetmenu(){
-	document.querySelectorAll(".all").forEach((element) => {
-		element.style = "background-color: rgb(252, 252, 252);";
-	});
-}
-
-function salvar() {
-	html2canvas(document.querySelector("#corpo"), {
-		"logging": true, //Habilita os logs
-		"useCORS": true
-	}).then(function(canvas) {
-		img.onload = () => {
-			img.onload = null;
-			document.getElementById("image").appendChild(img);
-		};
-		canvas.style = "width: 100%; height: 100%;";
-		img.src = canvas.toDataURL("image/png");
-		//document.getElementById("image").appendChild(img);
-
-		download.href = img.src.replace("image/png", "image/octet-stream");
-		download.setAttribute("target","blank");
-		download.download = "DrawnImage.png";
-	});
-	document.getElementById("image").style = "top: 2vh";
-	document.getElementById("save-buttons").hidden = false;
-}
-
-
-//cancelar download
-document.getElementById("cancelar").addEventListener("click", () => {
-	document.getElementById("image").style = "top: -1200px";
-	document.getElementById("save-buttons").hidden = true;
-});
-//salvar 
-document.getElementById("salvar").addEventListener("click", () => {
-	download.click();
-	document.getElementById("image").style = "top: -1200px";
-	img.src = "";
-	download.href = "index.html";
-	document.getElementById("save-buttons").hidden = true;
-});
-
-init();
+main();
